0b0cbf43d777d1c60f5484ab29c5b57609f73a09
Larry Knibb <larry.knibb@gmail.com>
2012-03-22 00:02:40 +0800

    v1 now updates the tile info when the app is loaded

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -11,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
 
 namespace BeijingAirQuality.WindowsPhone
 {
@@ -26,14 +28,42 @@ namespace BeijingAirQuality.WindowsPhone
 
         void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            if (NetworkInterface.GetIsNetworkAvailable())
-            {
-                Browser.Navigate(new Uri("http://iphone.bjair.info"));
-            }
-            else
+            if (!NetworkInterface.GetIsNetworkAvailable())
             {
                 MessageBox.Show("No Internet connection.");
+                return;
             }
+
+            DownloadBeijingAirQualityData();
+            Browser.Navigate(new Uri("http://iphone.bjair.info"));
+        }
+
+        void DownloadBeijingAirQualityData()
+        {
+            WebClient webClient = new WebClient();
+            webClient.OpenReadCompleted += new OpenReadCompletedEventHandler(webClient_OpenReadCompleted);
+            webClient.OpenReadAsync(new Uri("http://iphone.bjair.info/m/beijing/mobile"));
+        }
+
+        void webClient_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            byte[] response = new byte[e.Result.Length];
+            if (e.Result.Read(response, 0, response.Length) == 0) // empty response :(
+                return;
+
+            string html = System.Text.Encoding.UTF8.GetString(response, 0, response.Length);
+            Regex pattern = new Regex("<div id=\"current-aqi\".*?<h1>(\\d+)</h1>.*?Time: (\\d?\\d:\\d\\d).*?<div id=\"status\".*?>(.*?)<div", RegexOptions.Singleline);
+            Match match = pattern.Match(html);
+            if (!match.Success) // couldn't extract the PM2.5 value :(
+                return;
+
+            ShellTile tile = ShellTile.ActiveTiles.First();
+            StandardTileData data = new StandardTileData
+            {
+                BackContent = "PM2.5: " + match.Groups[1].Value + "\n" + match.Groups[3].Value.Trim(),
+                BackTitle = "Updated: " + match.Groups[2].Value
+            };
+            tile.Update(data);
         }
     }
 }
