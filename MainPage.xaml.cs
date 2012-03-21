e1494d5e32aa03d758c53e3fbb327577909af03f
Larry Knibb <larry.knibb@gmail.com>
2012-03-21 20:48:08 +0800

    added WebBrowser control to app

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -19,6 +20,20 @@ namespace BeijingAirQuality.WindowsPhone
         public MainPage()
         {
             InitializeComponent();
+
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (NetworkInterface.GetIsNetworkAvailable())
+            {
+                Browser.Navigate(new Uri("http://iphone.bjair.info"));
+            }
+            else
+            {
+                MessageBox.Show("No Internet connection.");
+            }
         }
     }
 }
